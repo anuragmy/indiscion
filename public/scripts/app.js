f1c81f50487ecdbca4c28113cfb215e48a3f8a50
Anurag <anuragmy2729@gmail.com>
2019-01-22 16:37:29 +0530

    'initial'

---
+++ public/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log('running');
+
+var template = React.createElement(
+  'h1',
+  null,
+  'Indiscion'
+);
+var appRoot = document.getElementById('root');
+
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,6 @@
+console.log('running');
+
+const template = <h1>Indiscion</h1>;
+const appRoot = document.getElementById('root');
+
+ReactDOM.render(template, appRoot);
